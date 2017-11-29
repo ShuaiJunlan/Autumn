66aead940103244bceeb9089ecc10f3128c194f9
shuaijunlan <shuaijunlan@gmail.com>
2017-11-29 20:47:55 +0800

    update

--- autumn-common/src/main/java/com/autumnframework/common/architect/constant/ResponseCode.java
+++ autumn-common/src/main/java/com/autumnframework/common/architect/constant/ResponseCode.java
@@ -6,25 +6,41 @@ package com.autumnframework.common.architect.constant;
  */
 public enum ResponseCode {
 
-    //  没有权限获取数据
-    GET_DATA_UNAUTHORIZED("3333", "unauthorized"),
-    //  请求成功
-    REQUEST_SUCCESS("1111", "successfully"),
     //  请求失败
     REQUEST_FAIL("0000", "fail"),
 
+
+    //  请求成功
+    REQUEST_SUCCESS(    "1111", "successfully"),
+    //  邮件发送成功
+    MAIL_SEND_SUCCESS(  "1001", "send mail successfully"),
+    //  认证成功
+    AUTH_SUCCESS(       "1002", "auth successfully"),
+    //  已认证
+    HAVE_AUTH(          "1003", "have authentication"),
+    //  邮箱验证成功
+    EMAIL_VALID(        "1004", "email valid"),
+
+
     //  请求没有授权
-    REQUEST_UNAUTHORIZED("3333", "fail"),
+    REQUEST_UNAUTHORIZED(   "3333", "fail"),
+    //  没有权限获取数据
+    GET_DATA_UNAUTHORIZED(  "3333", "unauthorized"),
+
+
+    //  邮件发送失败
+    MAIL_SEND_FAIL(     "4001", "send mail fairly"),
+    //  认证失败
+    AUTH_FAIL(          "4002", "auth fairly"),
+    //  链接失效
+    AUTH_LINK_TIMEOUT(  "4003", "link failure"),
+    //  邮箱验证失败
+    EMAIL_NOT_VALID(    "4004", "email not valid"),
+
 
     // 数据已存在
-    DATA_EXIT("5000", "data exist"),
-
-    MAIL_SEND_FAIL("4001", "send mail fairly"),
-    MAIL_SEND_SUCCESS("1001", "send mail successfully"),
-    AUTH_SUCCESS("1002", "auth successfully"),
-    HAVE_AUTH("1003", "have authentication"),
-    AUTH_FAIL("4002", "auth fairly"),
-    AUTH_LINK_TIMEOUT("4003", "link failure");
+    DATA_EXIT("5000", "data exist")
+    ;
 
     private String code;
     private String msg;
